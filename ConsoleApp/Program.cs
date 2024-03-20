63b718669cc8b6d99a863c41a9c460d975451176
Garti <67469783+Szowek@users.noreply.github.com>
2024-03-20 19:30:57 +0100

    fixed wrong path to file

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,7 +12,7 @@
         static void Main(string[] args)
         {
             var reader = new DataReader();
-            reader.ImportAndPrintData("dataa.csv");
+            reader.ImportAndPrintData("data.csv"); // changed "dataa.csv" to "data.csv", due to wrong path to file
         }
     }
 }
